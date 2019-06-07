3ea5ce3c23591139e60ba4a67d2b977f3810588a
tomoyotomo1201@gmail.com <tomoyotomo1201@gmail.com>
2019-06-07 22:09:15 +0900

    Port修正

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get('/',function(req, res){
 	res.sendFile(__dirname +'/client/index.html');
 });
 app.use('/client',express.static(__dirname+ '/client'));
-
-serv.listen(2000);
+const PORT = process.env.PORT 
+serv.listen(PORT);
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
